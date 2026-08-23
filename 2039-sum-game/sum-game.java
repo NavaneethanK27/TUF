c9ec61ec79319fddd46bc310cf682b29041eb0ce
Navaneethan K <navaneethannaveen2023@gmail.com>
2026-08-23 23:52:58 +0530

    Time: 7 ms (79.56%) | Memory: 46.8 MB (91.24%) - LeetSync

---
+++ 2039-sum-game/sum-game.java
@@ -0,0 +1,42 @@
+class Solution {
+    public boolean sumGame(String num) {
+        int n = num.length();
+        int mid = n / 2;
+
+        int leftSum = 0;
+        int rightSum = 0;
+
+        int leftQ = 0;
+        int rightQ = 0;
+
+        for (int i = 0; i < mid; i++) {
+            char ch = num.charAt(i);
+
+            if (ch == '?') {
+                leftQ++;
+            } else {
+                leftSum += ch - '0';
+            }
+        }
+
+        for (int i = mid; i < n; i++) {
+            char ch = num.charAt(i);
+
+            if (ch == '?') {
+                rightQ++;
+            } else {
+                rightSum += ch - '0';
+            }
+        }
+
+        int diff = leftSum - rightSum;
+        int qDiff = leftQ - rightQ;
+
+        // Alice wins if the difference cannot be balanced
+        if ((qDiff % 2) != 0) {
+            return true;
+        }
+
+        return diff != -(qDiff / 2) * 9;
+    }
+}
